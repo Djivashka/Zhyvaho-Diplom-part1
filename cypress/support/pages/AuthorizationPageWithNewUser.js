5ba11f40fd9a67cffc69d5dacd4f82a1b2211254
Yaroslav Zhyvaho <mrdjivashka@gmail.com>
2022-11-23 11:02:53 +0200

    exam part1

--- cypress/support/pages/AuthorizationPageWithNewUser.js
+++ cypress/support/pages/AuthorizationPageWithNewUser.js
@@ -1,6 +1,4 @@
-///<reference types="cypress"/>
 import {faker} from '@faker-js/faker';
-
 
 let user = {
     email: faker.internet.email(),
@@ -8,25 +6,30 @@ let user = {
     answer: faker.name.firstName('female')
 }
 
-it('Registration', () => {
-    cy.log("**Open registration page**")
+class AuthorizationPage {
+
+    registration(){
     cy.visit('/#/register')
-
-    cy.log('**Dismiss pop-up**')
     cy.get('.mat-focus-indicator.close-dialog.mat-raised-button.mat-button-base.mat-primary.ng-star-inserted').click()
-
-    cy.log("**Fill the registration page**")
     cy.get('#emailControl').type(user.email)
     cy.get('#passwordControl').type(user.password)
     cy.get('#repeatPasswordControl').type(user.password)
     cy.get('.mat-select-arrow-wrapper.ng-tns-c130-11').click()
     cy.get('#mat-option-7').click()
     cy.get('#securityAnswerControl').type(user.answer)
-
-    cy.log("**Click registration button**")
     cy.get('#registerButton').click()
-
-    cy.log("Check that registration was completed successfully")
-    cy.get('span.mat-simple-snack-bar-content').should('contain', 'Registration completed successfully. You can now log in.')
+    }
+    
+    login(){
+        cy.visit('/#/login')
+        cy.get('#email').type(user.email)
+        cy.get('#password').type(user.password)
+        cy.get('mat-icon.mat-icon.notranslate.material-icons.mat-ligature-font.mat-icon-no-color').eq(5).click()
+    }
 }
-)
+
+export default new AuthorizationPage();
+
+
+
+
